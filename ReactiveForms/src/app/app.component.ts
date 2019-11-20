b99f6d2bdb200fed2f7b67139f233cdd0d8a2a4a
Gautham.S <Gautham.S@stackroute.in>
2019-11-20 10:30:43 +0530

    Used Form Groups and Nesting Form Groups

---
+++ ReactiveForms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ReactiveForms';
+
+  registrationForm = new FormGroup({
+    name: new FormControl('Dravid'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  });
+
+}
